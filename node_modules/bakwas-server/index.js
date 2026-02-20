848832e25807f46dffb546fb3d384633baac185f
Shubham Mirashi <136989957+Shubham1905s@users.noreply.github.com>
2026-02-20 21:51:20 +0530

    audit fix

--- node_modules/bakwas-server/index.js
+++ node_modules/bakwas-server/index.js
@@ -1,15 +1,39 @@
+// import app from "./app.js";
+// import { connectDatabase } from "./db.js";
+
+// const PORT = process.env.PORT || 5000;
+
+// connectDatabase()
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log(`Server running on http://localhost:${PORT}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.error("MongoDB connection error:", error.message);
+//     process.exit(1);
+//   });
+
+
 import app from "./app.js";
 import { connectDatabase } from "./db.js";
 
-const PORT = process.env.PORT || 5000;
+let isConnected = false;
 
-connectDatabase()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error("MongoDB connection error:", error.message);
-    process.exit(1);
-  });
+const connectDBOnce = async () => {
+  if (!isConnected) {
+    try {
+      await connectDatabase();
+      console.log("MongoDB connected");
+      isConnected = true;
+    } catch (error) {
+      console.error("MongoDB connection error:", error.message);
+    }
+  }
+};
+
+// Vercel serverless handler
+export default async function handler(req, res) {
+  await connectDBOnce();
+  return app(req, res);
+}
