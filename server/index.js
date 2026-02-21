2abdfad0fd33b394df627e417c76433b4103f46c
Shubham Mirashi <136989957+Shubham1905s@users.noreply.github.com>
2026-02-21 22:06:38 +0530

    captcha solved

--- server/index.js
+++ server/index.js
@@ -1,39 +1,12 @@
-// import app from "./app.js";
-// import { connectDatabase } from "./db.js";
-
-// const PORT = process.env.PORT || 5000;
-
-// connectDatabase()
-//   .then(() => {
-//     app.listen(PORT, () => {
-//       console.log(`Server running on http://localhost:${PORT}`);
-//     });
-//   })
-//   .catch((error) => {
-//     console.error("MongoDB connection error:", error.message);
-//     process.exit(1);
-//   });
-
-
 import app from "./app.js";
-import { connectDatabase } from "./db.js";
 
-let isConnected = false;
+const PORT = process.env.PORT || 5000;
+const isVercelRuntime = Boolean(process.env.VERCEL);
 
-const connectDBOnce = async () => {
-  if (!isConnected) {
-    try {
-      await connectDatabase();
-      console.log("MongoDB connected");
-      isConnected = true;
-    } catch (error) {
-      console.error("MongoDB connection error:", error.message);
-    }
-  }
-};
-
-// Vercel serverless handler
-export default async function handler(req, res) {
-  await connectDBOnce();
-  return app(req, res);
+if (!isVercelRuntime) {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
 }
+
+export default app;
